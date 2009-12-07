2b7adc097ac081861900c73bdcc73f90253efc7a
Daniel Graziotin <daniel.graziotin@stud-inf.unibz.it>
2009-12-07 11:08:43 +0000

    Added class Helper for database connections
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009-10/ait/group2/trunk@31 dc6aa11a-a73b-4401-8366-add7e515eabc

---
+++ src/it/unibz/pomodroid/persistency/DBHelper.java
@@ -0,0 +1,90 @@
+package it.unibz.pomodroid.persistency;
+
+import java.io.File;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import com.db4o.config.Configuration;
+
+public class DBHelper {
+
+	private static ObjectContainer database;
+	private Context context;
+
+	public DBHelper(Context context) {
+		DBHelper.database = null;
+		this.context = context;
+	}
+
+	public void setDatabase(ObjectContainer database) {
+		DBHelper.database = database;
+	}
+
+	public ObjectContainer getDatabase() {
+		try {
+			if (database == null || database.ext().isClosed())
+				database = Db4o.openFile(dbConfig(), db4oDBFullPath(context));
+			return database;
+		} catch (Exception e) {
+			Log.e(DBHelper.class.getName(), e.toString());
+			return null;
+		}
+	}
+
+	/**
+	 * @return the context
+	 */
+	public Context getContext() {
+		return context;
+	}
+
+	/**
+	 * @param context
+	 *            the context to set
+	 */
+	public void setContext(Context context) {
+		this.context = context;
+	}
+
+	private Configuration dbConfig() {
+		Configuration configuration = Db4o.newConfiguration();
+		return configuration;
+	}
+
+	/**
+	 * @param context
+	 * @return
+	 */
+	public String db4oDBFullPath(Context context) {
+		return context.getDir("data", 0) + "/" + "android.db4o";
+	}
+
+	/**
+	 * Close database connection
+	 */
+	public void close() {
+		if (database != null) {
+			database.close();
+			database = null;
+		}
+	}
+
+	public void deleteDatabase() {
+		close();
+		new File(db4oDBFullPath(context)).delete();
+	}
+
+	public void backup(String path) {
+		getDatabase().ext().backup(path);
+	}
+
+	public void restore(String path) {
+		deleteDatabase();
+		new File(path).renameTo(new File(db4oDBFullPath(context)));
+		new File(path).delete();
+	}
+
+}
