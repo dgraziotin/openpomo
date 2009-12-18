e90c21e38a05b31e600977fc0b33f7d697b8297a
Daniel Graziotin <daniel.graziotin@stud-inf.unibz.it>
2009-12-18 14:14:13 +0000

    forgot to add test.xml
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009-10/ait/group2/trunk@54 dc6aa11a-a73b-4401-8366-add7e515eabc

--- src/it/unibz/pomodroid/Pomodroid.java
+++ src/it/unibz/pomodroid/Pomodroid.java
@@ -4,6 +4,7 @@ import it.unibz.pomodroid.persistency.DBHelper;
 import it.unibz.pomodroid.persistency.User;
 import it.unibz.pomodroid.services.TrackTicketFetcher;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,20 +17,24 @@ public class Pomodroid extends Activity {
 	private DBHelper dbHelper;
 	private Button button;
 	private TextView textView;
+	private Context context;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		dbHelper = new DBHelper(this);
+		this.context = this;
 		this.button = (Button) findViewById(R.id.Button01);
-		TextView textView = (TextView) findViewById(R.id.TextView01);
+		this.textView = (TextView) findViewById(R.id.TextView01);
 		
 		this.button.setOnClickListener(new OnClickListener() {
 			  @Override
 			  public void onClick(View v) {
 			    finish();
+			    Intent intent = new Intent(context, PomodroidTest.class);
+			    context.startActivity(intent);
 			  }
-			});
+		});
 		
 		User user = User.retrieve(dbHelper);
 		TrackTicketFetcher.fetch(user, dbHelper);
