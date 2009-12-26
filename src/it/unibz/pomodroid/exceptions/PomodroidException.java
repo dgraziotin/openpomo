c6591a68f6a3e2f8160aa4098c96ba7e77d49c1b
Daniel Graziotin <daniel.graziotin@stud-inf.unibz.it>
2009-12-26 10:32:22 +0000

    added PomodroidException Class
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009-10/ait/group2/trunk@71 dc6aa11a-a73b-4401-8366-add7e515eabc

---
+++ src/it/unibz/pomodroid/exceptions/PomodroidException.java
@@ -0,0 +1,23 @@
+package it.unibz.pomodroid.exceptions;
+public class PomodroidException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// Default constructor 
+	// initializes custom exception variable to none
+	public PomodroidException() {
+		// call superclass constructor
+		super();            
+	}
+
+	// Custom Exception Constructor
+	public PomodroidException(String message) {
+		// Call super class constructor
+		super(message);  
+	} 
+
+
+
+}
