a245647ca09c1b4514086058003a7fbe16693245
Thomas Schievenin <thomas.schievenin@stud-inf.unibz.it>
2010-01-06 16:09:29 +0000

    Bug fixed and features added
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009-10/ait/group2/trunk@133 dc6aa11a-a73b-4401-8366-add7e515eabc

--- src/it/unibz/pomodroid/DeleteActivity.java
+++ src/it/unibz/pomodroid/DeleteActivity.java
@@ -1,5 +1,7 @@
 package it.unibz.pomodroid;
 
+import it.unibz.pomodroid.exceptions.PomodroidException;
+import it.unibz.pomodroid.persistency.Event;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,7 +20,15 @@ public class DeleteActivity extends SharedActivity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		if (v.getId() == R.id.ButtonDeleteActivity) {
-			//
+			try {
+				it.unibz.pomodroid.persistency.Activity.deleteAll(dbHelper);
+				Event.deleteAll(dbHelper);
+				throw new PomodroidException("All activities and events deleted!","INFO");
+			} catch (PomodroidException e) {
+				e.alertUser(context);
+			}finally{
+				dbHelper.close();
+			}
 		}
 	}
 	
