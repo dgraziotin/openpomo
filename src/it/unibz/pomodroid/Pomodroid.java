18bb2a53638690e019cb195bc6a3dca97f2c15b6
Thomas Schievenin <thomas.schievenin@stud-inf.unibz.it>
2010-01-07 18:38:51 +0000

    labels changed, and coolest interface done
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009-10/ait/group2/trunk@142 dc6aa11a-a73b-4401-8366-add7e515eabc

--- src/it/unibz/pomodroid/Pomodroid.java
+++ src/it/unibz/pomodroid/Pomodroid.java
@@ -1,17 +1,13 @@
 package it.unibz.pomodroid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class Pomodroid extends SharedActivity implements OnClickListener {
+public class Pomodroid extends SharedActivity {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+<<<<<<< HEAD:src/it/unibz/pomodroid/Pomodroid.java
 <<<<<<< HEAD:src/it/unibz/pomodroid/Pomodroid.java
 		setContentView(R.layout.main);
 <<<<<<< HEAD:src/it/unibz/pomodroid/Pomodroid.java
@@ -119,6 +115,8 @@ public class Pomodroid extends SharedActivity implements OnClickListener {
 	public void onResume(){
 		super.onResume();
 =======
+=======
+>>>>>>> 01271d6... labels changed, and coolest interface done:src/it/unibz/pomodroid/Pomodroid.java
 		}	
 >>>>>>> 81881a4... Refactoring and cleanup, bugs fixed:src/it/unibz/pomodroid/Pomodroid.java
 	}
@@ -164,3 +162,60 @@ public class Pomodroid extends SharedActivity implements OnClickListener {
 =======
 >>>>>>> 19e0939... First version of enhanced use of SharedActivity:src/it/unibz/pomodroid/Pomodroid.java
 }
+=======
+		setContentView(R.layout.pomodroid);
+		/*
+		TextView textView = (TextView) findViewById(R.id.hello);
+		textView.setText("Pomodroid");
+		Button buttonAIS = (Button) findViewById(R.id.ButtonAIS);
+		buttonAIS.setOnClickListener((OnClickListener) this);
+		Button buttonTTS = (Button) findViewById(R.id.ButtonTTS);
+		buttonTTS.setOnClickListener((OnClickListener) this);
+		Button buttonTS = (Button) findViewById(R.id.ButtonTS);
+		buttonTS.setOnClickListener((OnClickListener) this);
+		Button buttonPreferences = (Button) findViewById(R.id.ButtonPreferences);
+		buttonPreferences.setOnClickListener((OnClickListener) this);
+		Button buttonTests = (Button) findViewById(R.id.ButtonTests);
+		buttonTests.setOnClickListener((OnClickListener) this);
+		Button buttonTRAC = (Button) findViewById(R.id.ButtonTrac);
+		buttonTRAC.setOnClickListener((OnClickListener) this);
+		Button buttonProm = (Button) findViewById(R.id.ButtonProm);
+		buttonProm.setOnClickListener((OnClickListener) this);
+		if (user == null) {
+			Intent intent = new Intent(this, Preferences.class);
+			startActivity(intent);
+		}	
+		*/
+
+	}
+
+	/*
+	@Override
+	public void onClick(View v) {
+		Intent i = null;
+		switch (v.getId()) {
+		case R.id.ButtonAIS:
+			i = new Intent(this, ActivityInventorySheet.class);
+			startActivity(i);
+			break;
+		case R.id.ButtonTTS:
+			i = new Intent(this, TodoTodaySheet.class);
+			startActivity(i);
+			break;
+		case R.id.ButtonTS:
+			i = new Intent(this, TrashSheet.class);
+			startActivity(i);
+			break;
+		case R.id.ButtonPreferences:
+			i = new Intent(this, Preferences.class);
+			startActivity(i);
+			break;
+		case R.id.ButtonTests:
+			i = new Intent(this, PomodroidTest.class);
+			startActivity(i);
+			break;
+
+		}
+	*/
+}
+>>>>>>> 2f6885d... labels changed, and coolest interface done:src/it/unibz/pomodroid/Pomodroid.java
